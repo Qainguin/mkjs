26d3eda1a3cf60af2d4cefdbc3c6fbd2e95104b3
Qainguin <slimaster14@gmail.com>
2024-08-11 23:40:39 -0500

    Added CSS file, moved course select creation function

---
+++ code/ui/courseUI.js
@@ -0,0 +1,29 @@
+//
+// courseUI.js
+//--------------------
+// Makes course select screen.
+// by Qainguin
+//
+//
+
+document.addEventListener('DOMContentLoaded', createCourseButtons);
+
+function createCourseButtons() {
+    const container = document.getElementById('courses');
+
+    if (container) {
+        // Loop to create 32 buttons
+        for (let i = 0; i < 32; i++) {
+            // Create a button element
+            const button = document.createElement('button');
+
+            button.setAttribute('onclick', `chooseCourse(${i})`);
+            button.className = 'courseCard';
+            button.style.backgroundImage = `url('resource/screenshots/${i}.png')`;
+            container.appendChild(button);
+            console.log("Creating course button...");
+        }
+    } else {
+        console.error("Container not found!");
+    }
+}
